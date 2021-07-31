87655f88f6f7d1f7bfd5f8fec7bfde92d5491bbf
annahaskelsky <83059442+annahaskelsky@users.noreply.github.com>
2021-07-31 23:43:55 +0300

    Initial commit

---
+++ js/book-controller.js
@@ -0,0 +1,144 @@
+'use strict'
+
+
+function onInit() {
+    renderBooks()
+    renderPages()
+    var currPage = getActivePage()
+    console.log(currPage);
+    document.querySelector(`.page-${currPage}`).classList.add('active-page')
+}
+
+function onSortBy(opt) {
+    sortBy(opt)
+    renderBooks()
+}
+
+function renderPages() {
+    var elPages = document.querySelector('.pages')
+    var numOfPages = getNumOfPages()
+    var strHTMLs = ''
+    for (var i = 1; i <= numOfPages; i++) {
+        strHTMLs += `<div class="page page-${i - 1}" onclick="onMoveToPage(${i - 1}, this)">${i}</div>`
+    }
+    elPages.innerHTML = strHTMLs
+}
+
+function onMoveToPage(pageNum, elPage) {
+    var activePage = getActivePage()
+    var elActivePage = document.querySelector(`.page-${activePage}`)
+    elActivePage.classList.remove('active-page')
+    elPage = elPage || document.querySelector(`.page-${pageNum}`)
+    elPage.classList.add('active-page')
+    moveToPage(+pageNum)
+    renderBooks()
+}
+
+function onAddBook() {
+    document.querySelector('.add').hidden = true
+    document.querySelector('.add-book').hidden = false
+}
+
+function onSubmitBook() {
+    var elBookName = document.querySelector('[name=title]')
+    var elPrice = document.querySelector('[name=price]')
+    var bookName = elBookName.value
+    var price = elPrice.value
+    if (bookName) addBook(bookName, price)
+    elBookName.value = ''
+    elPrice.value = ''
+    document.querySelector('.add-book').hidden = true
+    document.querySelector('.add').hidden = false
+    renderPages()
+    onMoveToPage(0)
+    renderBooks()
+}
+
+function renderBooks() {
+    var books = getBooks()
+    var strHTMLs = ''
+    books.forEach(function (book) {
+        strHTMLs += `<tr>
+        <td>${book.id}</td>
+        <td class="book-title">${book.name}</td>
+        <td>${book.price}</td>
+        <td>${book.rating}</td>
+        <td><button class="action-btn read" onclick="onReadBook('${book.id}')">Read</button></td>
+        <td><button class="action-btn update" onclick="onUpdateBook('${book.id}')">Update</button></td>
+        <td><button class="action-btn delete" onclick="onDeleteBook('${book.id}')">Delete</button></td>
+        </tr>`
+    })
+    var elTBody = document.querySelector('tbody')
+    elTBody.innerHTML = strHTMLs
+}
+
+function onDeleteBook(bookId) {
+    deleteBook(bookId)
+    renderPages()
+    var activePage = getActivePage()
+    onMoveToPage(activePage)
+    renderBooks()
+}
+
+function onUpdateBook(bookId) {
+    var newPrice = +prompt('New price?')
+    updateBook(bookId, newPrice)
+    renderBooks()
+}
+
+function onReadBook(bookId) {
+    var book = getBookById(bookId)
+    var elModal = document.querySelector('.modal')
+    elModal.querySelector('h5').innerText = book.name
+    elModal.querySelector('h6').innerText = `₪${book.price}`
+    elModal.querySelector('img').src = `img/${(book.name).split(' ').join('-')}.jpg`
+    elModal.querySelector('p').innerText = book.desc
+    elModal.style.display = 'block'
+}
+
+function onUpdateRating() {
+    var elBookName = document.querySelector('.modal').querySelector('h5').innerText
+    var elRating = +document.querySelector('.rating').innerText
+    onCloseModal()
+    updateRating(elBookName, elRating)
+    renderBooks()
+    document.querySelector('.rating').innerText = '0'
+}
+
+function onNextPage() {
+    var prevPage = getActivePage()
+    document.querySelector(`.page-${prevPage}`).classList.remove('active-page')
+    nextPage()
+    var activePage = getActivePage()
+    document.querySelector(`.page-${activePage}`).classList.add('active-page')
+    renderBooks()
+}
+
+function onPrevPage() {
+    var currPage = getActivePage()
+    document.querySelector(`.page-${currPage}`).classList.remove('active-page')
+    prevPage()
+    var activePage = getActivePage()
+    document.querySelector(`.page-${activePage}`).classList.add('active-page')
+    renderBooks()
+}
+
+function onRatingDown() {
+    var elRating = document.querySelector('.rating')
+    var newRating;
+    if (+elRating.innerText === 0) return
+    newRating = +elRating.innerText - 1
+    elRating.innerText = newRating
+}
+
+function onRatingUp() {
+    var elRating = document.querySelector('.rating')
+    var newRating;
+    if (+elRating.innerText === 10) return
+    newRating = +elRating.innerText + 1
+    elRating.innerText = newRating
+}
+
+function onCloseModal() {
+    document.querySelector('.modal').style.display = 'none'
+}
---
+++ js/services/book-service.js
@@ -0,0 +1,127 @@
+'use strict'
+const KEY = 'bookDB'
+const PAGE_SIZE = 5
+var gPageIdx = 0
+var gActivePage = 0
+var gSortBy = 'none'
+
+
+var gBooks
+var gBookNames = ['Harry Potter', 'Lord Of The Rings', 'Don Quixote', 'Alice\'s Adventures in Wonderland', 'Pinocchio', 'The Great Gatsby', 'Moby Dick', 'War and Peace', 'Hamlet', 'Pride and Prejudice', 'Crime and Punishment', 'Lord of the Flies']
+
+_createBooks()
+
+function sortBy(opt) {
+    gSortBy = opt
+}
+
+function getNumOfPages() {
+    return Math.ceil(gBooks.length / PAGE_SIZE)
+}
+
+function updateRating(bookName, newRating) {
+    var book = gBooks.find(function (book) {
+        return bookName === book.name
+    })
+    book.rating = newRating
+    _saveBooksToStorage()
+}
+
+function getBookNames() {
+    return gBookNames
+}
+
+function getActivePage() {
+    return gActivePage
+}
+
+function moveToPage(pageNum) {
+    console.log(pageNum);
+    gPageIdx = pageNum
+    gActivePage = pageNum
+}
+
+function nextPage() {
+    var numOfPages = getNumOfPages()
+    if (gPageIdx === numOfPages-1) return
+    gPageIdx++
+    gActivePage = gPageIdx
+    if (gPageIdx * PAGE_SIZE >= gBooks.length) gPageIdx = 0
+}
+
+function prevPage() {
+    if (!gPageIdx) return
+    gPageIdx--
+    gActivePage = gPageIdx
+
+}
+
+function getBooks() {
+    var startIdx = gPageIdx * PAGE_SIZE
+    console.log(gBooks);
+    if(gSortBy === 'title') {
+        gBooks.sort(function(book1, book2) {
+            return (book1.name > book2.name) ? 1 : -1
+        })
+    } else if(gSortBy === 'price') {
+        gBooks.sort(function(book1, book2) {
+            return book1.price - book2.price
+        })
+    } 
+    return gBooks.slice(startIdx, startIdx + PAGE_SIZE)
+}
+
+function deleteBook(bookId) {
+    var bookIdx = gBooks.findIndex(function (book) {
+        return bookId === book.id
+    })
+    gBooks.splice(bookIdx, 1)
+    _saveBooksToStorage()
+}
+
+function addBook(bookName, price) {
+    var book = _createBook(bookName, price)
+    gBooks.unshift(book)
+    _saveBooksToStorage()
+}
+
+function getBookById(bookId) {
+    var book = gBooks.find(function (book) {
+        return bookId === book.id
+    })
+    return book
+}
+
+function updateBook(bookId, newPrice) {
+    var book = gBooks.find(function (book) {
+        return bookId === book.id
+    })
+    book.price = newPrice
+    _saveBooksToStorage()
+}
+
+function _createBook(bookName, price) {
+    return {
+        id: makeId(),
+        name: bookName,
+        price: price || getRandomIntInclusive(30, 100),
+        rating: 0,
+        desc: makeLorem()
+    }
+}
+
+function _createBooks() {
+    var books = loadFromStorage(KEY)
+    if (!books || !books.length) {
+        books = []
+        gBookNames.forEach(function (bookName) {
+            books.push(_createBook(bookName))
+        })
+    }
+    gBooks = books
+    _saveBooksToStorage()
+}
+
+function _saveBooksToStorage() {
+    saveToStorage(KEY, gBooks)
+}
